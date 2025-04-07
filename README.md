81eae46c5e3110178b2409dde3863aa2bc877ce9
HesoRider <206319354+HesoRider@users.noreply.github.com>
2025-04-07 22:53:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# はじめに
+去年、TVアニメ「[しかのこのこのここしたんたん](https://www.anime-shikanoko.jp/)」にめちゃハマりました。
+特にOPめっちゃすこです。
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZZvIVRQ4E7I?si=0AfGrTTBhboIIWYa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+これを題材に、「しかのこのこのここしたんたん」という文字列がマルコフ連鎖で現れる過程を示した動画がありました。
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Xkq13ZthmA0?si=BuQi3_-5U9yKlYH6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+非常に面白い。というわけで自分でも実装してみようと試みたのがこのリポジトリになります。
+
+# 仕様
+`shikanoko.cpp`をコンパイル・実行すると`output.txt`がディレクトリ内に生成され、結果がファイル内に出力されます。
+
+# 注意事項
+ソースコードの複製・改造等は自由にお楽しみください。<br>
+プログラムを実行したことによって発生したいかなる問題に対しては一切の責任を負いかねます。すべて自己責任でご利用ください。
+
+Copyright (C) 2025 ヘソライダー All Rights Reserved.
---
+++ shikanoko.cpp
@@ -0,0 +1,69 @@
+/*
+Copyright (C) 2025 ヘソライダー All Rights Reserved.
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+// しかのこでマルコフ連鎖
+int next(int index)
+{
+    // 文字の順番で判断
+    int next_index = 7;
+    srand((unsigned)time(NULL));
+    switch (index)
+    {
+    case 0: // し
+        if (rand() % 2 == 0) next_index = 1; // か
+        else next_index = 4; // た
+        break;
+    case 1: // か
+        next_index = 2; // の
+        break;
+    case 2: // の
+        next_index = 3; // こ
+        break;
+    case 3: // こ
+        if (rand() % 3 == 0) next_index = 0; // し
+        else if (rand() % 3 == 1) next_index = 2; // の
+        else next_index = 4; // た
+        break;
+    case 4: // た
+        next_index = 5; // ん
+        break;
+    case 5: // ん
+        if (rand() % 2 == 0) next_index = 4; // た
+        else next_index = 7; // おわり
+        break;
+    default:
+        break;
+    }
+
+    return next_index;
+}
+
+int main()
+{
+    // 6つの文字から一つ選ぶ
+    vector<string> shikanoko = {"し", "か", "の", "こ", "た", "ん"};
+
+    // テキストファイルに結果を書き込み
+    ofstream file("output.txt");
+    if (!file)
+    {
+        cout << "No File!" << endl;
+        exit(1);
+    }
+
+    int index = 0;
+    while (index < 7)
+    {
+        file << shikanoko[index];
+        index = next(index);
+    }
+    file.close();
+}
